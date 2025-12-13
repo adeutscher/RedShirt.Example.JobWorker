b977ccd49b5869fdc24348bd999e267cf0e03c66
Alan Deutscher <adeutscher@gmail.com>
2025-12-13 02:56:15 -0800

    NuGet updates, remove no-longer-needed S3 call

--- src/RedShirt.Example.JobWorker.Common.Aws/Extensions/ServiceCollectionExtensions.cs
+++ src/RedShirt.Example.JobWorker.Common.Aws/Extensions/ServiceCollectionExtensions.cs
@@ -1,6 +1,5 @@
 using Amazon.Extensions.NETCore.Setup;
 using Amazon.Runtime;
-using Amazon.S3;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace RedShirt.Example.JobWorker.Common.Aws.Extensions;
@@ -17,18 +16,10 @@ public static class ServiceCollectionExtensions
                 .AddAWSService<TService>();
         }
 
-        // Note: S3 needs a special carve-out for AmazonS3Config.ForcePathStyle
-        //          in order to avoid DNS troubles
-        if (typeof(TService) == typeof(IAmazonS3))
-        {
-            var s3Config = new AmazonS3Config
-            {
-                ServiceURL = url,
-                ForcePathStyle = true
-            };
-
-            return services.AddSingleton<IAmazonS3>(_ => new AmazonS3Client(s3Config));
-        }
+        /*
+         * Note: Not needed here, but S3 needs a special carve-out for AmazonS3Config.ForcePathStyle
+         *           in order to avoid DNS troubles
+         */
 
         Console.WriteLine($"Using AWS service URL: {url}");
 
