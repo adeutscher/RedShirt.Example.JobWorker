e39383d062e2fe5f4d42cd79e9ef0c6c547967d8
Alan Deutscher <adeutscher@gmail.com>
2025-12-15 22:32:17 -0800

    Comment adjustment

--- src/RedShirt.Example.JobWorker.JobManagement.Common/Services/SourceMessageSorter.cs
+++ src/RedShirt.Example.JobWorker.JobManagement.Common/Services/SourceMessageSorter.cs
@@ -14,8 +14,13 @@ internal class SourceMessageSorter : ISourceMessageSorter
 {
     public List<IJobModel> GetSortedListOfJobs(List<IJobModel> input)
     {
-        // The default template does not sort the list at all,
-        // but this class was made to be a central, ideal place to do it.
+        /*
+         * The default template does not sort the list at all,
+         * but this class was made to be a central, ideal place to do it.
+         */
+        
+        // When working with multiple worker threads,
+        // it's ideal wants to sort the larger jobs to process first  
         return input;
     }
 }
