d8649865bdce65784352cd9d4c755f7b40ee5369
Alan Deutscher <adeutscher@gmail.com>
2026-07-31 03:12:45 -0700

    comment

--- src/RedShirt.Example.JobWorker.Common.Distributed/Services/DistributedSleepService.cs
+++ src/RedShirt.Example.JobWorker.Common.Distributed/Services/DistributedSleepService.cs
@@ -2,8 +2,10 @@ namespace RedShirt.Example.JobWorker.Common.Distributed.Services;
 
 /// <summary>
 ///     Abstraction of sleeping for Distributed services.
-///     Copied from Core's <c>ISleepService</c> to avoid a circular project dependency for such a lightweight service.
-///     Its use prevents certain unit tests from greatly increasing test times in Docker builds, CI/CD pipelines, or a developer's manual tests.
+///     Copied from Core's <c>ISleepService</c> to avoid
+///     a circular project dependency or creating an entirely new subproject for such a lightweight service.
+///     Its use prevents certain unit tests from greatly increasing test times in Docker builds, CI/CD pipelines, or a
+///     developer's manual tests.
 /// </summary>
 public interface IDistributedSleepService
 {
