246e623fe163cd58fbe962db22308175bfd79ca2
Alan Deutscher <adeutscher@gmail.com>
2026-07-31 04:53:18 -0700

    housekeeping

--- src/RedShirt.Example.JobWorker.Common.Distributed/Services/Redis/RedisLockService.cs
+++ src/RedShirt.Example.JobWorker.Common.Distributed/Services/Redis/RedisLockService.cs
@@ -1,6 +1,7 @@
 using Medallion.Threading.Redis;
 using Microsoft.Extensions.Options;
 using RedShirt.Example.JobWorker.Common.Distributed.Configuration;
+using RedShirt.Example.JobWorker.Common.Distributed.Exceptions;
 using RedShirt.Example.JobWorker.Common.Distributed.Models;
 using RedShirt.Example.JobWorker.Common.Distributed.Services.Abstractions;
 
@@ -21,24 +22,40 @@ internal class RedisLockService(
     {
         var redis = await retryWrapper.RunAsync(redisConnectionCacheService.GetDatabaseAsync, cancellationToken);
         var redisLock = new RedisDistributedLock(lockName, redis);
-        return new DistributedLock(await redisLock.TryAcquireAsync(Timeout, cancellationToken));
+        return new DistributedLock(retryWrapper, await redisLock.TryAcquireAsync(Timeout, cancellationToken));
     }
 
-    private sealed class DistributedLock(RedisDistributedLockHandle? lockHandle) : IAbstractedLock
+    private sealed class DistributedLock(
+        IDistributedRetryWrapperService retryWrapper,
+        RedisDistributedLockHandle? lockHandle) : IAbstractedLock
     {
         public bool IsAcquired => lockHandle is not null;
 
-        public Task UnlockAsync(CancellationToken cancellationToken = default)
+        public async Task UnlockAsync(CancellationToken cancellationToken = default)
         {
             cancellationToken.ThrowIfCancellationRequested();
 
             // ReSharper disable once ConvertIfStatementToReturnStatement
             if (lockHandle is null)
             {
-                return Task.CompletedTask;
+                return;
             }
 
-            return lockHandle.DisposeAsync().AsTask();
+            try
+            {
+                await retryWrapper.RunAsync(_ => lockHandle.DisposeAsync().AsTask(), cancellationToken);
+            }
+            catch (WorkerDistributedException)
+            {
+                /*
+                 * Pass if the retry fails
+                 * Nothing really to do. We're throwing away the lock anyway.
+                 *
+                 * I haven't been able to reproduce an exception here in local testing
+                 * since I've switched Unlock to be UnlockAsync,
+                 * but I have no reason to believe that it couldn't do it again.
+                 */
+            }
         }
     }
 }
