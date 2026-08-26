4bf51c15812798683143a00d4e8f93135e8a5fe0
Alan Deutscher <adeutscher@gmail.com>
2026-08-26 11:36:12 -0700

    Local testing: Try sourcing RabbitMQ creds from env vars.

--- test/local/make-local-rabbitmq-resources.py
+++ test/local/make-local-rabbitmq-resources.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 
+import os
+
 import pika
 
 queue_name = 'RabbitQueue'
 
-credentials = pika.PlainCredentials('foo', 'bar')
+credentials = pika.PlainCredentials(
+    os.environ.get('RABBITMQ_DEFAULT_USER', 'foo'),
+    os.environ.get('RABBITMQ_DEFAULT_PASS', 'bar'),
+)
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='localhost', credentials=credentials)
 )
